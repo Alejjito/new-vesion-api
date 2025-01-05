36b69428328297192a55f365755e6911f9991388
Alejjito <rincona486@gmail.com>
2025-01-05 15:32:52 -0500

    refactor: remove PicoAndPlaca model and add new models for Exception, ExceptionType, PeakAndPlate, PeakAndPlatesWeekday, Restriction, and RestrictionType

--- app/models/peak_and_plate.ts
+++ app/models/peak_and_plate.ts
@@ -3,7 +3,7 @@ import { BaseModel, column, hasOne } from '@adonisjs/lucid/orm'
 import City from './city.js'
 import type { HasOne } from '@adonisjs/lucid/types/relations'
 
-export default class PicoAndPlaca extends BaseModel {
+export default class PeakAndPlate extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
